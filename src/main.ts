1ce3a839d0cdb50091d29997decdd51c8c927b5a
Bari Ademi <baroademi@gmail.com>
2024-05-06 00:14:46 +0400

    CORS TEST

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,20 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin,X-Requested-With,Content-Type,Accept',
-    );
-    next();
-  });
-  app.enableCors({
-    origin: true,
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    credentials: true,
-  });
+  const app = await NestFactory.create(AppModule,{cors:true});
+  // app.use(function (req, res, next) {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header(
+  //     'Access-Control-Allow-Headers',
+  //     'Origin,X-Requested-With,Content-Type,Accept',
+  //   );
+  //   next();
+  // });
+  // app.enableCors({
+  //   origin: true,
+  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  //   credentials: true,
+  // });
 
   await app.listen(3000);
 }
