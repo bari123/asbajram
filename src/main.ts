17983dc0cfba01e0778c0b9477cff603fd8f4fd2
Bari Ademi <baroademi@gmail.com>
2024-05-06 01:15:17 +0400

    CORS TEST 5

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,9 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // another common pattern
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET,PATCH,DELETE,POST,PUT,OPTIONS',
-    );
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-    );
-    next();
-  });
+
   app.enableCors({
-    origin: 'https://autoservicebajram.netlify.app',
+    origin: '*',
     methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTION'],
     credentials: true,
   });
