cbceea55a1ff6621afd0fd28a018c239d4bde2d1
Bari Ademi <baroademi@gmail.com>
2024-05-05 23:50:18 +0400

    THIRD

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
 
   await app.listen(3000);
