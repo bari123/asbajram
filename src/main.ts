404865c7ec442a7ed6e6ad5224123bd3bde2467c
Bari Ademi <baroademi@gmail.com>
2024-05-06 00:09:16 +0400

    CORS TEST

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin,X-Requested-With,Content-Type,Accept',
+    );
+    next();
+  });
   app.enableCors({
     origin: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
