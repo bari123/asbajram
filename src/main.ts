24ae89bab60b1b775d057b3a763943a78a139f4a
Bari Ademi <baroademi@gmail.com>
2024-05-05 23:47:37 +0400

    THIRD

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
-    credentials: true, // Enable sending credentials (cookies) with requests if needed
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
   });
 
   await app.listen(3000);
